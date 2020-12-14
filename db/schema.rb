7e9ff4464e228683e4132fc340c1f16bdcdce14a
Nikos Agathos <nicolas.agathos@gmail.com>
2020-12-14 21:00:59 +0100

    pokemon model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_12_14_195751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.integer "height"
+    t.integer "weight"
+    t.string "species"
+    t.integer "hp"
+    t.integer "attack"
+    t.integer "defense"
+    t.integer "special_attack"
+    t.integer "special_defence"
+    t.integer "speed"
+    t.string "sprite_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "abilities", default: [], array: true
+    t.string "types", default: [], array: true
+  end
 
 end
