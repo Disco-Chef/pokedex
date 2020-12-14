8c4dcbabaa40aa872593b8f5b6cf90bae7571e48
Nikos Agathos <nicolas.agathos@gmail.com>
2020-12-14 21:08:57 +0100

    first, seed one successfully, then, test pokemon creation when calling the eventual service object

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PokemonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test first do manually the seeding, then add test for when it's
+  # test "pokemon objects created successfully" do
+  #   pokemon = Pokemon.new(...)
   # end
 end
