46343abd84805a2bb288decb81351b3973be103c
Nikos Agathos <nicolas.agathos@gmail.com>
2020-12-17 23:54:51 +0100

    before i begin, let this be a good merge point, in case of breaks 😬

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 Pokemon.destroy_all
 Type.destroy_all
 
-puts "Fetching data from Pokeapi and creating Pokemon..."
+puts "Fetching data from Pokeapi and creating Pokemon... Feel free to grab a ☕, this should take about 4-5 minutes ⏱ ;)"
 # pass however many pokemon you want to create from PokeApi. No arguments means that the "regular" pokemon will be created (meaning up to id 898)
 CreateFromPokeapi.new.create_pokemon()
 puts "Seeding complete! Created #{Pokemon.count} Pokemon out of 893"
+
